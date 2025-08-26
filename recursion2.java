67910a6dc00c5faf2d818a4563159b656746cf75
Gautam29013 <jonygautam29013@gmail.com>
2025-08-26 11:22:10 +0530

    Implement recursive methods for finding character occurrences, reversing strings, and checking sorted arrays

---
+++ recursion2.java
@@ -0,0 +1,15 @@
+public class recursion2 {
+    public static void recursion(int n , String src , String helper , String dest) {
+        if(n == 1){
+            System.out.println("transfer disk " + n + " from " +src +"to " +dest);
+            return;
+        }
+        recursion(n-1 ,src,dest,helper);
+        System.out.println("transfer disk " + n + " from " +src +"to " +dest);
+        recursion(n-1 , helper ,src,dest);
+    }
+    public static void main(String[] args) {
+        int n = 4;  
+        recursion(n ,"S" ,"H" , "D");
+    }
+}
